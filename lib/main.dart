50fce748783f9cc92ee5b491b35df41c755835c9
RotenKiwi <joshiparth212@gmail.com>
2023-12-16 23:06:30 +0530

    photo click page

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,17 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:stylesynth/screens/landing.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+  runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Landing()
-    );
-  }
+      home: Landing()));
 }
--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:camera/camera.dart';
-import 'package:path_provider/path_provider.dart';
 
 import '../Components/DressCard.dart';
 
